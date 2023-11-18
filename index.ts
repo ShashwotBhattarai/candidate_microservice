f7a80d82e09f117431b9a93d08be06c321e618ca
ShashwotBhattarai <shashwot.tech@gmail.com>
2023-11-19 03:34:06 +0545

    refactor

--- index.ts
+++ index.ts
@@ -4,16 +4,18 @@ import authRoute from "./routes/auth.routes";
 import connectToDatabase from "./database/db.connect";
 import uploadCandidateInfoRoute from "./routes/uploadCandidateInfo.route";
 import getCandidateInfoRoute from "./routes/getCadidateInfoRoute";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 app.use(bodyParser.json());
 
 connectToDatabase();
 app.use("/auth", authRoute);
-app.use("/upload", uploadCandidateInfoRoute)
-app.use("/getCandidateInfo",getCandidateInfoRoute)
+app.use("/upload", uploadCandidateInfoRoute);
+app.use("/getCandidateInfo", getCandidateInfoRoute);
 
 app.listen(port, () => {
   console.log(`Gateway is running on port ${port}`);
--- routes/uploadCandidateInfo.route.ts
+++ routes/uploadCandidateInfo.route.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, Router } from "express";
-import { validateCandidateInfo } from "../validators/uploadCandidateInfo.validate";
+import { validateCandidate } from "../validators/uploadCandidateInfo.validate";
 import { checkFileMiddleware } from "../middlewares/checkFile.middleware";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import multer from "multer";
@@ -14,23 +14,19 @@ const router: Router = express.Router();
 router.post(
   "/",
   upload.single("cv"),
-  authMiddleware(["candidate"]),
+  // authMiddleware(["candidate"]),
+  validateCandidate,
   checkFileMiddleware,
   async (req: Request, res: Response) => {
-    const { error } = validateCandidateInfo.validate(req.body);
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
-    }
-    
-    const saveUserDetailsServiceResponse= await saveUserDetailsToDatabase(
-      req.file,
-      req.body,
-      req.headers.authorization || ""
-    );
-    res.status(saveUserDetailsServiceResponse.status).json(saveUserDetailsServiceResponse.message);
+    // const saveUserDetailsServiceResponse= await saveUserDetailsToDatabase(
+    //   req.file,
+    //   req.body,
+    //   req.headers.authorization || ""
+    // );
+    // res.status(saveUserDetailsServiceResponse.status).json(saveUserDetailsServiceResponse.message);
 
-    
-
+    console.log(req.file);
+    res.status(200).json("ok");
   }
 );
 
