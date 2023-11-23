82098889a3bcd67a9fe2a1bb0385537a4267441e
ShashwotBhattarai <shashwot.tech@gmail.com>
2023-11-23 17:26:11 +0545

    deleted auth files

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import authRoute from "./routes/auth.routes";
 import connectToDatabase from "./database/db.connect";
 import uploadCandidateInfoRoute from "./routes/uploadCandidateInfo.route";
 import getCandidateInfoRoute from "./routes/getCadidateInfoRoute";
@@ -13,7 +12,6 @@ const port = 4000;
 app.use(bodyParser.json());
 
 connectToDatabase();
-app.use("/auth", authRoute);
 app.use("/upload", uploadCandidateInfoRoute);
 app.use("/getCandidateInfo", getCandidateInfoRoute);
 
