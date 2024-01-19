899a9080912fb835dea3f3fc467873ee8e5cbfd9
shashwotbhattarai <shashwot.tech@gmail.com>
2024-01-20 04:50:38 +0545

    stopped tracking dist folder

--- dist/src/constants/email.templets.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CVUploadedEmailTemplate = void 0;
-exports.CVUploadedEmailTemplate = {
-    subject: "Your CV has been uploaded",
-    text: "Dear Candidate your CV has been uploaded successfully",
-};
